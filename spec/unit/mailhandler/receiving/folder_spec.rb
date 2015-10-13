c74b90f35eab9fa9c14b9a3f5f683220a5be2c57
Igor Balos <ibalosh@gmail.com>
2015-10-13 14:30:45 +0200

    added additonal folder check tests

--- spec/unit/mailhandler/receiving/folder_spec.rb
+++ spec/unit/mailhandler/receiving/folder_spec.rb
@@ -16,7 +16,7 @@ describe MailHandler::Receiving::FolderChecker do
 
     context '.find email' do
 
-      context 'email not found' do
+      context 'not found' do
 
         it 'result' do
 
@@ -24,34 +24,58 @@ describe MailHandler::Receiving::FolderChecker do
 
         end
 
-        it 'found emails' do
+        it 'by subject' do
 
           checker.find({:by_subject => 'test123'})
           expect(checker.found_emails).to be_empty
 
         end
-
+        
       end
 
-      context 'email found' do
+      context 'found' do
+
+        let(:mail1) { Mail.read_from_string(File.read "#{data_folder}/email1.txt")}
+        let(:mail2) { Mail.read_from_string(File.read "#{data_folder}/email2.txt")}
 
         it 'result' do
 
-          expect(checker.find({:by_subject => 'test 12345'})).to be true
+          expect(checker.find({:by_subject => mail1.subject})).to be true
 
         end
 
-        it 'found emails - single' do
+        it 'by subject - single' do
 
-          checker.find({:by_subject => 'test 12345'})
-          expect(checker.found_emails.size).to be 1
+          checker.find({:by_subject => mail1.subject})
+
+          aggregate_failures "found mail details" do
+            expect(checker.found_emails.size).to be 1
+            expect(checker.found_emails.first).to eq mail1
+            expect(checker.found_emails).not_to include mail2
+          end
 
         end
 
-        it 'found emails - multiple' do
+        it 'by subject - multiple' do
 
           checker.find({:by_subject => 'test'})
-          expect(checker.found_emails.size).to be 2
+
+          aggregate_failures "found mail details" do
+            expect(checker.found_emails.size).to be 2
+            expect(checker.found_emails).to include mail1
+            expect(checker.found_emails).to include mail2
+          end
+
+        end
+
+        it 'by subject and count - multiple' do
+
+          checker.find({:by_subject => 'test', :count => 1})
+
+          aggregate_failures "found mail details" do
+            expect(checker.found_emails.size).to be 1
+            expect(checker.found_emails).to include mail2
+          end
 
         end
 
