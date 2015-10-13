a73e1470fdbf342d32f6537f082d0974b923bcc4
Igor Balos <ibalosh@gmail.com>
2015-10-13 17:06:57 +0200

    added more folder check tests

--- spec/unit/mailhandler/receiving/folder_spec.rb
+++ spec/unit/mailhandler/receiving/folder_spec.rb
@@ -16,6 +16,17 @@ describe MailHandler::Receiving::FolderChecker do
 
     context '.find email' do
 
+      context 'search options' do
+
+        it 'default' do
+
+          checker.find({:by_subject => 'test'})
+          expect(checker.search_options).to eq({:count=>50, :archive=>false, :by_subject=>'test'})
+
+        end
+
+      end
+
       context 'not found' do
 
         it 'result' do
@@ -34,6 +45,13 @@ describe MailHandler::Receiving::FolderChecker do
         it 'by date' do
 
           checker.find({:by_date => Time.new(2015,10,14,13,30,0, "+02:00")})
+          expect(checker.found_emails).to be_empty
+
+        end
+
+        it 'by recipient' do
+
+          checker.find({:by_recipient => { to: 'igor@example.com'} })
           expect(checker.found_emails).to be_empty
 
         end
@@ -75,7 +93,7 @@ describe MailHandler::Receiving::FolderChecker do
 
         end
 
-        it 'by subject and count - multiple' do
+        it 'by count' do
 
           checker.find({:by_subject => 'test', :count => 1})
           expect(checker.found_emails.size).to be 1
@@ -86,6 +104,59 @@ describe MailHandler::Receiving::FolderChecker do
 
           checker.find({:by_date => Time.new(2015,10,12,13,30,0, "+02:00")})
           expect(checker.found_emails.size).to be 2
+
+        end
+
+        it 'by recipient' do
+
+          checker.find({:by_recipient => { to: 'igor1@example.com'} })
+          expect(checker.found_emails.size).to be 1
+
+          checker.find({:by_recipient => { to: 'igor2@example.com'} })
+          expect(checker.found_emails.size).to be 1
+
+        end
+
+        context 'archiving emails' do
+
+          after(:each) {
+
+            FileUtils.rm_r "#{data_folder}/checked", :force => true
+
+          }
+
+          let(:mail) {
+
+            mail = Mail.read_from_string(File.read "#{data_folder}/email2.txt")
+            mail.subject = 'test 872878278'
+            File.write("#{data_folder}/email3.txt", mail)
+            mail
+
+          }
+
+          let(:checker_archive) { MailHandler::Receiving::FolderChecker.new(data_folder, "#{data_folder}/checked") }
+
+          it 'to same folder - delete' do
+
+            checker.find({:by_subject => mail.subject, :archive => true})
+            expect(checker.found_emails.size).to be 1
+
+            checker.find({:by_subject => mail.subject, :archive => true})
+            expect(checker.found_emails).to be_empty
+
+
+          end
+
+          it 'to separate folder' do
+
+            checker_archive.find({:by_subject => mail.subject, :archive => true})
+            expect(checker_archive.found_emails.size).to be 1
+
+            checker_archive.find({:by_subject => mail.subject, :archive => true})
+            expect(checker_archive.found_emails).to be_empty
+
+
+          end
 
         end
 
