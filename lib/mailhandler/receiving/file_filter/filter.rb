17bab785efd83889a970e1b1bdecf3d5318a34a0
Igor Balos <ibalosh@gmail.com>
2015-12-21 13:37:35 +0100

    moved filtering for folder search to separate namespace

--- lib/mailhandler/receiving/file_filter/filter.rb
+++ lib/mailhandler/receiving/file_filter/filter.rb
@@ -1,28 +1,136 @@
 require 'fileutils'
+require_relative 'base'
 
-module Filter
+module MailHandler
 
-  # Base filtering class, which is used for reading list of all files based on passed pattern.
-  # Patterns to be used can be checked here: http://ruby-doc.org/core-1.9.3/Dir.html
-  class Base
+  module Receiving
 
-    def Base.sort(files)
+    module Filter
 
-      swapped = true
-      j = 0
+      class ByFilename < Base
 
-      while swapped do
+        def get(pattern)
 
-        swapped = false
-        j+=1
+          super(pattern)
 
-        (files.size - j).times do |i|
+        end
 
-          if File.new(files[i]).ctime < File.new(files[i + 1]).ctime
-            tmp = files[i]
-            files[i] = files[i + 1]
-            files[i + 1] = tmp
-            swapped = true
+      end
+
+      class ContentBase < Base
+
+        def initialize(content)
+
+          @content = content.to_s
+
+        end
+
+        protected
+
+        def filter_files(files)
+
+          files.select do |file|
+
+            begin
+
+              file_match_filter?(file)
+
+            rescue
+
+              # return false if error occurred or content was not found
+              false
+
+            end
+
+          end
+
+        end
+
+        def file_match_filter?(file)
+
+          raise StandardError, 'Not implemented'
+
+        end
+
+      end
+
+      class BySubject < ContentBase
+
+        def initialize(content)
+
+          super(content)
+
+        end
+
+        def get(pattern)
+
+          files = super(pattern)
+          filter_files(files)
+
+        end
+
+        private
+
+        def file_match_filter?(file)
+
+          read_email(File.read(file)).subject.include? @content
+
+        end
+
+      end
+
+      class ByContent < ContentBase
+
+        def initialize(content)
+
+          super(content)
+
+        end
+
+        def get(pattern)
+
+          files = super(pattern)
+          filter_files(files)
+
+        end
+
+        private
+
+        def file_match_filter?(file)
+
+          File.read(file).include? @content
+
+        end
+
+      end
+
+      class ByDate < ContentBase
+
+        def initialize(date)
+
+          @date = date
+
+        end
+
+        def get(pattern)
+
+          files = super(pattern)
+          filter_files(files)
+
+        end
+
+        private
+
+        def file_match_filter?(file)
+
+          file = File.new(file)
+          if file != nil
+
+            file.ctime > @date
+
+          else
+
+            false
 
           end
 
@@ -30,182 +138,33 @@ module Filter
 
       end
 
-      files
+      module Email
 
-    end
+        class ByRecipient < ContentBase
 
-    def get(pattern)
+          def initialize(recipient)
 
-      files = []
-      Dir.glob(pattern).each { |file| files << File.absolute_path(file) }
-      files
+            @recipient = recipient
 
-    end
+          end
 
-    protected
+          def get(pattern)
 
-    def read_email(content)
+            files = super(pattern)
+            filter_files(files)
 
-      Mail.read_from_string(content)
+          end
 
-    end
+          private
 
-  end
+          def file_match_filter?(file)
 
-  class ByFilename < Base
+            email = read_email(File.read(file))
+            email[@recipient.keys.first].to_s.include? @recipient.values.first
 
-    def get(pattern)
-
-      super(pattern)
-
-    end
-
-  end
-
-  class ContentBase < Base
-
-    def initialize(content)
-
-      @content = content.to_s
-
-    end
-
-    protected
-
-    def filter_files(files)
-
-      files.select do |file|
-
-        begin
-
-          file_match_filter?(file)
-
-        rescue
-
-          # return false if error occurred or content was not found
-          false
+          end
 
         end
-
-      end
-
-    end
-
-    def file_match_filter?(file)
-
-      raise StandardError, 'Not implemented'
-
-    end
-
-  end
-
-  class BySubject < ContentBase
-
-    def initialize(content)
-
-      super(content)
-
-    end
-
-    def get(pattern)
-
-      files = super(pattern)
-      filter_files(files)
-
-    end
-
-    private
-
-    def file_match_filter?(file)
-
-      read_email(File.read(file)).subject.include? @content
-
-    end
-
-  end
-
-  class ByContent < ContentBase
-
-    def initialize(content)
-
-      super(content)
-
-    end
-
-    def get(pattern)
-
-      files = super(pattern)
-      filter_files(files)
-
-    end
-
-    private
-
-    def file_match_filter?(file)
-
-      File.read(file).include? @content
-
-    end
-
-  end
-
-  class ByDate < ContentBase
-
-    def initialize(date)
-
-      @date = date
-
-    end
-
-    def get(pattern)
-
-      files = super(pattern)
-      filter_files(files)
-
-    end
-
-    private
-
-    def file_match_filter?(file)
-
-      file = File.new(file)
-      if file != nil
-
-        file.ctime > @date
-
-      else
-
-        false
-
-      end
-
-    end
-
-  end
-
-  module Email
-
-    class ByRecipient < ContentBase
-
-      def initialize(recipient)
-
-        @recipient = recipient
-
-      end
-
-      def get(pattern)
-
-        files = super(pattern)
-        filter_files(files)
-
-      end
-
-      private
-
-      def file_match_filter?(file)
-
-        email = read_email(File.read(file))
-        email[@recipient.keys.first].to_s.include? @recipient.values.first
 
       end
 
