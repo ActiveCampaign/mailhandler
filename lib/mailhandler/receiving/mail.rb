14b9d5c2e6185f9e4091fe5e7435104bf10cc32e
Igor Balos <ibalosh@gmail.com>
2016-02-11 13:10:00 +0100

    optimised a bit imap search

--- lib/mailhandler/receiving/mail.rb
+++ lib/mailhandler/receiving/mail.rb
@@ -8,39 +8,47 @@ module Mail
 
       options = validate_options(options)
       options[:read_only] ? imap.examine(options[:mailbox]) : imap.select(options[:mailbox])
-
       uids = imap.uid_search(options[:keys])
-      uids.reverse! if options[:what].to_sym == :last
-      uids = uids.first(options[:count]) if options[:count].is_a?(Integer)
-      uids.reverse! if (options[:what].to_sym == :last && options[:order].to_sym == :asc) ||
-          (options[:what].to_sym != :last && options[:order].to_sym == :desc)
 
+      if uids.nil? || uids.empty?
 
-      if block_given?
-
-        uids.each do |uid|
-          uid = options[:uid].to_i unless options[:uid].nil?
-          fetchdata = imap.uid_fetch(uid, ['RFC822'])[0]
-          new_message = Mail.new(fetchdata.attr['RFC822'])
-          new_message.mark_for_delete = true if options[:delete_after_find]
-          if block.arity == 3
-            yield new_message, imap, uid
-          else
-            yield new_message
-          end
-          imap.uid_store(uid, "+FLAGS", [Net::IMAP::DELETED]) if options[:delete_after_find] && new_message.is_marked_for_delete?
-          break unless options[:uid].nil?
-        end
-
-        imap.expunge if options[:delete_after_find]
+        []
 
       else
 
-        emails = []
+        uids.reverse! if options[:what].to_sym == :last
+        uids = uids.first(options[:count]) if options[:count].is_a?(Integer)
+        uids.reverse! if (options[:what].to_sym == :last && options[:order].to_sym == :asc) ||
+            (options[:what].to_sym != :last && options[:order].to_sym == :desc)
 
-        unless uids.nil?
+        if block_given?
 
           uids.each do |uid|
+
+            uid = options[:uid].to_i unless options[:uid].nil?
+            fetchdata = imap.uid_fetch(uid, ['RFC822'])[0]
+            new_message = Mail.new(fetchdata.attr['RFC822'])
+            new_message.mark_for_delete = true if options[:delete_after_find]
+
+            if block.arity == 3
+              yield new_message, imap, uid
+            else
+              yield new_message
+            end
+
+            imap.uid_store(uid, "+FLAGS", [Net::IMAP::DELETED]) if options[:delete_after_find] && new_message.is_marked_for_delete?
+            break unless options[:uid].nil?
+
+          end
+
+          imap.expunge if options[:delete_after_find]
+
+        else
+
+          emails = []
+
+          uids.each do |uid|
+
             uid = options[:uid].to_i unless options[:uid].nil?
             fetchdata = imap.uid_fetch(uid, ['RFC822'])[0]
             emails << Mail.new(fetchdata.attr['RFC822'])
@@ -49,10 +57,10 @@ module Mail
 
           end
 
-        end
+          imap.expunge if options[:delete_after_find]
+          emails.size == 1 && options[:count] == 1 ? emails.first : emails
 
-        imap.expunge if options[:delete_after_find]
-        emails.size == 1 && options[:count] == 1 ? emails.first : emails
+        end
 
       end
 
@@ -61,15 +69,15 @@ module Mail
     # Start an IMAP session
     def connect(config=Mail::Configuration.instance)
 
-        @imap = Net::IMAP.new(settings[:address], settings[:port], settings[:enable_ssl], nil, false)
+      @imap = Net::IMAP.new(settings[:address], settings[:port], settings[:enable_ssl], nil, false)
 
-        if settings[:authentication].nil?
-          imap.login(settings[:user_name], settings[:password])
-        else
-          # Note that Net::IMAP#authenticate('LOGIN', ...) is not equal with Net::IMAP#login(...)!
-          # (see also http://www.ensta.fr/~diam/ruby/online/ruby-doc-stdlib/libdoc/net/imap/rdoc/classes/Net/IMAP.html#M000718)
-          imap.authenticate(settings[:authentication], settings[:user_name], settings[:password])
-        end
+      if settings[:authentication].nil?
+        imap.login(settings[:user_name], settings[:password])
+      else
+        # Note that Net::IMAP#authenticate('LOGIN', ...) is not equal with Net::IMAP#login(...)!
+        # (see also http://www.ensta.fr/~diam/ruby/online/ruby-doc-stdlib/libdoc/net/imap/rdoc/classes/Net/IMAP.html#M000718)
+        imap.authenticate(settings[:authentication], settings[:user_name], settings[:password])
+      end
 
     end
 
