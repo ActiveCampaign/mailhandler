240241fae3f83d8bbc7d60b39c62b221f5b3bfc5
Igor Balos <ibalosh@gmail.com>
2018-02-15 17:11:14 +0100

    allow setting up timeouts for sending by Postmark API

--- lib/mailhandler/sending/api.rb
+++ lib/mailhandler/sending/api.rb
@@ -11,37 +11,34 @@ module MailHandler
       attr_accessor :host,
                     :api_token,
                     :use_ssl,
-                    :client
+                    :client,
+                    :http_open_timeout,
+                    :http_read_timeout
 
       def initialize(api_token = nil)
-
         @type = :postmark_api
         @host = DEFAULT_HOST
         @api_token = api_token
         @use_ssl = false
 
+        @http_open_timeout = 15
+        @http_read_timeout = 15
       end
 
       def send(email)
-
         verify_email(email)
         init_client
         client.deliver_message(email)
-
       end
 
       def init_client
-
         @client = setup_sending_client
-
       end
 
       def setup_sending_client
-
         # clearing cache so valid host is accepted, and not the cached one
         Postmark::HttpClient.instance_variable_set('@http', nil)
-        Postmark::ApiClient.new(api_token, http_open_timeout: 15, host: host, secure: @use_ssl)
-
+        Postmark::ApiClient.new(api_token, http_open_timeout: http_open_timeout, http_read_timeout: http_read_timeout, host: host, secure: @use_ssl)
       end
 
       protected
--- lib/mailhandler/version.rb
+++ lib/mailhandler/version.rb
@@ -1,5 +1,3 @@
 module MailHandler
-
-  VERSION = '1.0.34'
-
+  VERSION = '1.0.35'
 end
